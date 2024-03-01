3a78f2f33e078f39ab2394a55ad036fadc839d39
EpsilonUp <134354816+EpsilonDown@users.noreply.github.com>
2024-03-01 17:07:49 +0900

    Update Setting.cs

--- Setting.cs
+++ Setting.cs
@@ -29,9 +29,48 @@ namespace PantheonOfRegions
 			"GG_Hollow_Knight",
 			"GG_Radiance"
         };
-		
-		public string Super = "Pantheon of";
-		public string Title = "Regions";
-		public string Desc = "Seek the Gods Attuned by the Regions";
+		public string ChangeText(string key, string sheetTitle, string orig) => key switch
+		{
+			"CustomBossDoorTitle" => "Pantheon of",
+			"CustomBossDoorSuper" => "Regions",
+			"CustomBossDoorDesc" => "Fight Gods Attuned through the Region",
+			"VENGEFLY_MAIN" => "Ascenders",
+            "VENGEFLY_SUB" => "",
+			"MOSSCHARGER_MAIN" => "Green Ambushers",
+            "MOSSCHARGER_SUB" => "",
+			"FAILED_CHAMPION_SUPER" => "Guardians of",
+            "FAILED_CHAMPION_MAIN" => "Crossroads",
+			"SISTERS_MAIN" => "Alliance",
+            "SISTERS_SUB" => "of Battle",
+			"ENRAGED_GUARDIAN_MAIN" => "Restless Guardians",
+            "ENRAGED_GUARDIAN_SUB" => "",
+			"SOUL_TYRANT_MAIN" => "Soul Masters",
+            "SOUL_TYRANT_SUB" => "",
+			"TRAITOR_LORD_MAIN" => "Queen's",
+            "TRAITOR_LORD_SUB" => "Tributes",
+			"NAILMASTERS_MAIN" => "The Nailmasters",
+            "NAILMASTERS_SUB" => "",
+			"UUMUU_MAIN" => "Blind Protectors",
+            "UUMUU_SUB" => "",
+			"NOSK_MAIN" => "Stalking Warriors",
+            "NOSK_SUB" => "",
+			"WHITE_DEFENDER_MAIN" => "Guardians of Waterways",
+            "WHITE_DEFENDER_SUB" => "",
+			"HORNET_MAIN" => "Stinger",
+            "HORNET_SUB" => "Knights",
+            "GOD_TAMER_SUPER" => "Champions of",
+            "GOD_TAMER_MAIN" => "Colosseum",
+			"GRUZ_MOTHER_MAIN" => "Lord of Flies",
+            "GRUZ_MOTHER_SUB" => "",
+            "GRIMM_NIGHTMARE_MAIN" => "Reapers of Dreams",
+            "GRIMM_NIGHTMARE_SUB" => "",
+            "PURE_VESSEL_MAIN" => "Void Vessels",
+			"ABSOLUTE_RADIANCE_MAIN" => "RADIANCE",
+            "ABOLUTE_RADIANCE_SUB" => "Mother of All Moths",
+				
+            _ =>orig
+
+		};
+
 	}
 }
