f5dd3ad706d487906e46287890c6a3476d0a40e1
EpsilonUp <134354816+EpsilonDown@users.noreply.github.com>
2024-03-01 15:41:27 +0900

    Update Setting.cs

--- Setting.cs
+++ Setting.cs
@@ -9,24 +9,29 @@ namespace PantheonOfRegions
 			"GG_Mega_Moss_Charger",
 			"GG_Failed_Champion",
 			"GG_Mantis_Lords_V",
+			"GG_Spa",
 			"GG_Crystal_Guardian_2",
 			"GG_Soul_Tyrant",
 			"GG_Traitor_Lord",
 			"GG_Nailmasters",
+			"GG_Spa",
 			"GG_Uumuu_V",
 			"GG_Nosk_V",
 			"GG_White_Defender",
 			"GG_Hornet_2",
+			"GG_Spa",
 			"GG_God_Tamer",
 			"GG_Collector",
 			"GG_Gruz_Mother",
 			"GG_Grimm_Nightmare",
+			"GG_Spa",
+			"GG_Wyrm",
 			"GG_Hollow_Knight",
 			"GG_Radiance"
         };
 		
 		public string Super = "Pantheon of";
 		public string Title = "Regions";
-		public string Desc = "Bosses together strong";
+		public string Desc = "Seek the Gods Attuned by the Regions";
 	}
 }
