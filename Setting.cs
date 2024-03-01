58c22b0cbfaec02f4a7bab3df51ba97244d15b17
EpsilonUp <134354816+EpsilonDown@users.noreply.github.com>
2024-03-01 15:37:49 +0900

    Update Setting.cs

--- Setting.cs
+++ Setting.cs
@@ -1,20 +1,32 @@
-﻿using System.Collections.Generic;
+
 namespace PantheonOfRegions
 {
     public class Setting
     {
 		
 		public List<string> PantheonRooms = new List<string> {
+			"GG_Vengefly_V",
+			"GG_Mega_Moss_Charger",
+			"GG_Failed_Champion",
+			"GG_Mantis_Lords_V",
+			"GG_Crystal_Guardian_2",
+			"GG_Soul_Tyrant",
 			"GG_Traitor_Lord",
-            "GG_Mantis_Lords_V"
+			"GG_Nailmasters",
+			"GG_Uumuu_V",
+			"GG_Nosk_V",
+			"GG_White_Defender",
+			"GG_Hornet_2",
+			"GG_God_Tamer",
+			"GG_Collector",
+			"GG_Gruz_Mother",
+			"GG_Grimm_Nightmare",
+			"GG_Hollow_Knight",
+			"GG_Radiance"
         };
 		
 		public string Super = "Pantheon of";
-
-		
 		public string Title = "Regions";
-
-		
 		public string Desc = "Bosses together strong";
 	}
 }
