ed4b78178dffe51816c94668e557e55ffb9cd8da
EpsilonUp <134354816+EpsilonDown@users.noreply.github.com>
2024-04-28 14:48:26 +0900

    Create Sly.cs

---
+++ BossFixes/Sly.cs
@@ -0,0 +1,56 @@
+namespace PantheonOfRegions.Behaviours
+{
+    internal class GreatNailsageSly : MonoBehaviour
+    {
+        private PlayMakerFSM _control;
+        
+        private void Awake()
+        {
+            _control = gameObject.LocateMyFSM("Control");
+        }
+
+        private IEnumerator Start()
+        {
+            _control.SetState("Init");
+
+            _control.Fsm.GetFsmBool("Final Rage").Value = true;
+            
+            _control.GetAction<FloatCompare>("Cyc Down").float2.Value = ArenaInfo.BottomY + 4;
+            _control.GetAction<FloatOperator>("Cyc Jump Launch").float1.Value = ArenaInfo.CenterX;
+            _control.GetAction<SetFloatValue>("Jump To L", 0).floatValue.Value = ArenaInfo.RightX - 8;
+            _control.GetAction<SetFloatValue>("Jump To L", 1).floatValue.Value = ArenaInfo.LeftX;
+            _control.GetAction<SetFloatValue>("Jump To R", 0).floatValue.Value = ArenaInfo.LeftX + 8;
+            _control.GetAction<SetFloatValue>("Jump To R", 1).floatValue.Value = ArenaInfo.RightX;
+            
+            _control.GetState("Bow").AddCoroutine(TweenOut);
+            _control.GetState("Stun Wait").AddMethod(() => _control.SendEvent("READY"));
+            _control.GetState("Grabbing").AddMethod(() => _control.SendEvent("GRABBED"));
+            
+            yield return new WaitWhile(() => _control.ActiveStateName != "Docile");
+
+            GameObject spinTink = new GameObject("Spin Tink");
+            var collider = spinTink.AddComponent<CircleCollider2D>();
+            spinTink.AddComponent<DamageHero>();
+            collider.isTrigger = true;
+            collider.radius = 3;
+            spinTink.transform.SetParent(gameObject.transform, false);
+            _control.Fsm.GetFsmGameObject("Spin Tink").Value = spinTink;
+
+            _control.SetState("Battle Start");
+        }
+
+        private IEnumerator TweenOut()
+        {
+            yield return new WaitForSeconds(5);
+
+            GetComponent<BoxCollider2D>().isTrigger = true;
+            yield return new WaitUntil(() =>
+            {
+                transform.Translate(Vector3.down * 25 * Time.deltaTime);
+                return transform.position.y <= ArenaInfo.BottomY - 10;
+            });
+            ColosseumManager.EnemyCount--;
+            Destroy(gameObject);
+        }
+    }
+}
