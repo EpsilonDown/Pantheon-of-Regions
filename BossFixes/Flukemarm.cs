cd790504b992dbe030a64a6695617a28bf4d7346
EpsilonUp <134354816+EpsilonDown@users.noreply.github.com>
2024-06-23 18:03:10 +0900

    Update Flukemarm.cs

--- BossFixes/Flukemarm.cs
+++ BossFixes/Flukemarm.cs
@@ -12,20 +12,17 @@ namespace PantheonOfRegions.Behaviours
             _mother = gameObject.LocateMyFSM("Fluke Mother");
         }
 
-        private IEnumerator Start()
+        private void Start()
         {
-            _mother.SetState("Init");
 
-            GameObject hatcherCage = Instantiate(PantheonOfRegions.GameObjects["Hatcher Cage"], transform.position, Quaternion.identity);
+            GameObject hatcherCage = Instantiate(PantheonOfRegions.GameObjects["hatchercage"], transform.position, Quaternion.identity);
             hatcherCage.SetActive(true);
             foreach (var collider in hatcherCage.GetComponents<BoxCollider2D>())
             {
                 Destroy(collider);
             }
+            _mother.Fsm.GetFsmGameObject("Cage").Value = hatcherCage;
 
-            yield return new WaitUntil(() => _mother.ActiveStateName == "Idle");
-
-            _mother.SetState("Roar End");
         }
     }
 }
