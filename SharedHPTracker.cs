5ae5db6031bfd84ec325903802ee7af9a37c9ca1
EpsilonUp <134354816+EpsilonDown@users.noreply.github.com>
2025-01-13 22:10:12 +0900

    Update SharedHPTracker.cs

--- SharedHPTracker.cs
+++ SharedHPTracker.cs
@@ -1,1 +1,28 @@
+using PantheonOfRegions.Behaviours;
+namespace PantheonOfRegions
+{
+    internal class SharedHPTracker : MonoBehaviour
+    {
+        private SharedHealthManager sharedhp;
 
+        private void Awake()
+        {
+            sharedhp = GetComponent<SharedHealthManager>();
+        }
+
+        private void Start()
+        {
+            string goName = gameObject.name;
+
+
+            if (goName.Contains("Cliff Howlers"))
+            {
+                sharedhp = 
+            }
+            else if (goName.Contains("Crossroads Dominators"))
+            {
+
+            }
+        }
+    }
+}
